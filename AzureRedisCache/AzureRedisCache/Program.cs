ab11e84a512fc8dc4d79f1b0e4a074aa046b9228
Victor Moreno <code.zemoreno@gmail.com>
2018-09-27 13:06:12 -0500

    Original Commit
    Original Commit

---
+++ AzureRedisCache/AzureRedisCache/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureRedisCache
+{
+    class Program
+    {
+        /*            
+         * Comments:
+         * 
+         * For run this code without errors, you should to create an Azure Redis Cache Service in Microsoft Azure, and get your
+         * own ConnectionString and passwords for the server .
+         * Once you have your Azure Redis Cache service, now you need to set the property "SSL Port " no enabled if you don't do
+         * this action, you can't run the code correctly.
+         * Don't forget to add the reference StackExchange.Redis from Nuget Package also.
+         * More references: 
+         * 
+         * API:
+         * https://github.com/StackExchange/StackExchange.Redis/blob/master/StackExchange.Redis/StackExchange/Redis/IDatabase.cs
+         * 
+         * One tool that you can use for monitoring your redis data base is: Redis Desktop Manager from:
+         * http://redisdesktop.com/
+         */
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Value = "", Key = "";
+                int Operation = 0;              
+
+                RedisCache ServerAzureRedisCache = new RedisCache();
+
+                do
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("* * * [ Azure Redis Cache Operations ] * * *");
+                    Console.WriteLine();
+
+                    Console.WriteLine(" 1) Save data.");
+                    Console.WriteLine(" 2) Search data.");
+                    Console.WriteLine(" 3) Update data.");
+                    Console.WriteLine(" 4) Delete data.");
+                    Console.WriteLine(" 5) List all data");
+                    Console.WriteLine(" 0) Exit.");
+
+                    Console.WriteLine();
+
+                    Console.Write("Choose operation: ");
+                    Operation = int.Parse(Console.ReadLine().Trim());
+                    Console.WriteLine();
+
+                    switch (Operation)
+                    {
+                        case 1:
+
+                            /*
+                             Code for save data in the Azure Redis Cache DataBase
+                            */
+
+                            Console.Write("Write one key to the data: ");
+                            Key = Console.ReadLine().Trim();
+
+                            Console.Write("Write one value to save: ");
+                            Value = Console.ReadLine().Trim();
+
+                            if(ServerAzureRedisCache.SaveData(Key, Value)==true)
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("Data saved successfully! [ Now press Enter ]");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("Unsaved data [ Now press Enter ]");
+                                Console.ReadLine();
+                            }
+
+                            break;
+
+                        case 2:
+
+                            /*
+                              Code for search data in the Azure Redis Cache DataBase
+                            */
+
+                            Console.Write("Write the Key to search: ");
+                            Key = Console.ReadLine().Trim();
+
+                            string  ValueFound = ServerAzureRedisCache.SearchData(Key);
+
+                            if (ValueFound!=null)
+                                Console.WriteLine("The data is: " + ValueFound + " [ Now press Enter ]");
+                    
+                            else
+                                Console.WriteLine("Data not found" + " [ Now press Enter ]");
+
+                            Console.ReadLine();
+                            
+                            break;
+
+                        case 3:
+
+                            /*
+                             Code for update data in the Azure Redis Cache DataBase
+                             */
+
+                            Console.Write("Write the Key to search: ");
+                            Key = Console.ReadLine().Trim();
+
+                            Console.Write("Write the new value: ");
+                            Value = Console.ReadLine().Trim();
+
+                            if (ServerAzureRedisCache.UpdateData(Key, Value) == true)
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("Data updated successfully! [ Now press Enter ]");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("Data was not updated [ Now press Enter ]");
+                                Console.ReadLine();
+                            }
+
+                            break;
+
+                        case 4:
+
+                            /*
+                             Code for delete data in the Azure Redis Cache DataBase
+                           */
+
+                            Console.Write("Write the Key to delete: ");
+                            Key = Console.ReadLine().Trim();
+
+                            bool ResultDelete = ServerAzureRedisCache.DeleteData(Key);
+
+                            if (ResultDelete == true)
+                                Console.WriteLine("The data was deleted successfully!" + " [ Now press Enter ]");
+
+                            else
+                                Console.WriteLine("Data not found" + " [ Now press Enter ]");
+
+                            Console.ReadLine();
+
+                            break;
+
+                        case 5:
+
+                            /*
+                             Code for list all data in the Azure Redis Cache DataBase
+                           */
+
+                            List<RedisObject> RedisObjects = ServerAzureRedisCache.ListKeys();
+
+                            if (RedisObjects.Count()>0)
+                            {
+                                foreach (RedisObject RedisHash in RedisObjects)
+                                {
+                                    Console.Write(RedisHash.Key + " : ");
+                                    Console.Write(RedisHash.Value);
+                                    Console.WriteLine();
+                                }
+
+                                Console.WriteLine("");
+                                Console.WriteLine("All hashes shown" + " [ Now press Enter ]");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("");
+                                Console.WriteLine("No information at this time" + " [ Now press Enter ]");
+                                Console.ReadLine();
+                            }
+                           
+
+                            break;
+
+                                           }
+            
+                } while (Operation!=0);
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("An error occurred. Detail: " + ex.Message + " [ Now press Enter ]");
+
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ AzureRedisCache/AzureRedisCache/RedisCache.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace AzureRedisCache
+{
+    public class RedisCache
+    {
+        public string HostName { get; set; }
+        public string ConnectionString { get; set; }
+        public int PortNoSSL { get; set; }
+
+        /// <summary>
+        /// The constructor of RedisCache Class has the connection string necessary to set connections with the Azure Cache Redis from Microsoft Azure.
+        /// </summary>
+        public RedisCache()
+        {
+            this.ConnectionString = 
+            //"myserverazurerediscache.redis.cache.windows.net,abortConnect=false,password=wU7KEJJy4v3iEH6e0M/Pe74MJSUIbTXyk9SXRMt18vM=";
+            "myrediserver.redis.cache.windows.net:6380,password=PKk1DRtF3PeESjv0+30wfP5M4NVy837s6OWBuyIOT1Q=,ssl=True,abortConnect=False";
+
+            this.HostName =
+            "myrediserver.redis.cache.windows.net";
+            
+            this.PortNoSSL = 6379;
+        }
+        /// <summary>
+        /// This method search data by one  key in the Azure Redis Cache DataBase
+        /// </summary>
+        /// <param name="Value">Data to search</param>
+        /// <returns>False if the data not exist, True if the data exist</returns>
+        public string SearchData(string Key)
+        {
+            try
+            {               
+                ConnectionMultiplexer Connection = ConnectionMultiplexer.Connect(this.ConnectionString);
+
+                var DataBase = Connection.GetDatabase();
+                var Data = DataBase.StringGet(Key);
+
+                return Data.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        /// <summary>
+        /// Thie method save data in the Azure Redis Cache DataBase by one Key.
+        /// </summary>
+        /// <param name="Key">Name of key</param>
+        /// <param name="Value">Value for the key</param>
+        /// 
+        public bool SaveData(string Key, string Value)
+        {
+            try
+            {
+                ConnectionMultiplexer Connection = ConnectionMultiplexer.Connect(this.ConnectionString);
+                var DataBase = Connection.GetDatabase();
+
+                return DataBase.StringSet(Key, Value);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+        /// <summary>
+        /// This method update an existing string by key
+        /// </summary>
+        /// <param name="Key">One existing key</param>
+        /// <param name="NewValue">New value to update</param>
+        /// <returns>True if exist the key, false if not exist the key</returns>
+        public bool UpdateData(string Key, string NewValue)
+        {
+            try
+            {
+                ConnectionMultiplexer Connection = ConnectionMultiplexer.Connect(this.ConnectionString);
+
+                var DataBase = Connection.GetDatabase();
+                var Data = DataBase.StringGet(Key);
+
+                if (Data.HasValue == true)
+                    return DataBase.StringSet(Key, NewValue);
+
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        /// <summary>
+        /// This method delete data in the Azure Redis Cache DataBase by one Key
+        /// </summary>
+        /// <param name="Key">One existing key</param>
+        /// <returns>True if exist key, false if not exist the key</returns>
+        public bool DeleteData(string Key)
+        {
+            try
+            {
+                ConnectionMultiplexer Connection = ConnectionMultiplexer.Connect(this.ConnectionString);
+                var DataBase = Connection.GetDatabase();
+
+                return DataBase.KeyDelete(Key);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+        /// <summary>
+        /// This method return all the strings existing in the Azure Redis Cache
+        /// </summary>
+        /// <returns>A list of objects type RedisObject</returns>
+        public List<RedisObject> ListKeys()
+        {
+            try
+            {
+                List<RedisObject> KeyList = new List<RedisObject>();
+
+                ConnectionMultiplexer Connection = ConnectionMultiplexer.Connect(this.ConnectionString);
+
+                var Server = Connection.GetServer(this.HostName + ":" + this.PortNoSSL);
+
+                var DataBase = Connection.GetDatabase();
+
+                foreach (var Key in Server.Keys())
+                {
+                    KeyList.Add(new RedisObject()
+                    {
+                        Key = Key,
+                        Value = DataBase.StringGet(Key),
+                    });
+                }
+
+                return KeyList;
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }     
+    }
+}
